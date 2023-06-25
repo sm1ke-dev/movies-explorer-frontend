47c5da96981f45fd24ae60538e7b792447c7276c
sm1ke <smike143@yandex.ru>
2023-06-26 00:17:19 +0500

    feat: создать секцию techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import "./Main.css";
 import Header from "../Header/Header";
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
 
 function Main() {
   return (
@@ -11,6 +12,7 @@ function Main() {
       <main className="main">
         <Promo />
         <AboutProject />
+        <Techs />
       </main>
     </>
   );

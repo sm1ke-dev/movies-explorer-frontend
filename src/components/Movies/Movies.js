f6e454cb0729d73fbfd5bd8d553c2ea7085d8fc9
sm1ke <smike143@yandex.ru>
2023-06-27 03:44:17 +0500

    feat: создать компонент SearchForm

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import SearchForm from "../SearchForm/SearchForm";
 
 function Movies() {
   return (
     <>
       <Header movies={true} />
-      <main className="movies"></main>
+      <main className="movies">
+        <SearchForm />
+      </main>
       <Footer />
     </>
   );
